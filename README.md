3a490abe973a89bdbfbb2ec32af2de3c11bfb9a9
hasstMac <hasstmac@hasstMacs-MacBook-Air.local>
2018-05-13 18:55:45 -0700

    added the remove/delete tests

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Mongo / Mocha Starter..
+# Mongo / Mongoose / Mocha Starter..
+
+
+npm run test
+
+## Tests
+- Connect to db
+- Save to db
+- Find in db
+- Find by id in db
+- Remove from db
---
+++ test/remove_test.js
@@ -0,0 +1,47 @@
+//// USE CASE ////
+// Create and save in db
+// Use findOneAndRemove to delete record from db
+// Try to findOne in the db (just removed)
+// assert that results is "NULL"
+
+/// SAVING TESTS ///
+const assert = require('assert');
+const Character = require('../models/character');
+
+// describe tests
+describe("deleting records", function(){
+  // be sure to pass in done...
+  // refactor the save and create
+  // a beforeEach function
+
+  var testChar;
+
+  beforeEach(function(done){
+    // save new character to db
+    testChar = new Character({
+      name: 'Timmy',
+      weight: 180
+    });
+    // use a promise to handle saving to db
+    testChar.save().then(function(){
+      // test if the char is new (return true if not yet in database,
+      // false if the character has been saved into the database)
+      assert(testChar.isNew === false);
+      done();
+    });
+  });
+
+  // find tests
+  it('delete one record from the db', function(done){
+    // find and delete the item from the db
+      Character.findOneAndRemove({ name: 'Timmy' }).then(function(){
+        // see if the one you just deleted is really gone
+        // if results is "NULL" = success...!
+        Character.findOne({ name: 'Timmy' }).then(function(result){
+          assert(result === null);
+          done();
+        });
+      });
+  });
+
+});
